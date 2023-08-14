da7b89a6f69915cfc5443edad3d27a38ad13bd1a
LuskaFer <luska.fernandes@live.com>
2023-08-14 10:28:31 -0300

    aulas do 29 ao 55

---
+++ ws-eclipse/curso_java/src/curso_java/testeDeFor.java
@@ -0,0 +1,35 @@
+package curso_java;
+
+import java.util.Scanner;
+
+public class testeDeFor {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		/*int N = sc.nextInt();
+		
+		int soma = 0;
+		for (int i = 0; i < N; i++ ) {
+			int x = sc.nextInt();
+			soma += x;
+			
+		}
+		System.out.println(soma);
+		
+		
+		for (int i=0; i<5; i++) {
+			System.out.println("Valor de i: " + i);
+			}
+		*/
+		
+		for (int i=4; i>=0; i--) {
+			System.out.println("Valor de i: " + i);
+			}
+		
+		
+		sc.close();
+	}
+
+}
